0fd41d21bf2debb7acd7e1cd480935e3c8ed8d4e
rzranz <rzranz07@gmail.com>
2026-07-09 17:56:08 +0700

    prod req

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const Api = axios.create({
   
-  baseURL: 'http://localhost:8000/api', 
+  baseURL: import.meta.env.VITE_API_URL || 'https://api.darulfikri.sch.id/api', 
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
