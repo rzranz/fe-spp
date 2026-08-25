2c9f112b09e69d1fe4562cc90f3bcf4c446c260e
rzranz <rzranz07@gmail.com>
2026-08-25 20:42:43 +0700

    feat: login using NISN

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -139,14 +139,14 @@ const handleLogin = async () => {
         <form class="space-y-6" @submit.prevent="handleLogin">
           <div>
             <label for="email" class="block text-sm font-medium text-slate-700">
-              Email address
+              Email / NISN
             </label>
             <div class="mt-1">
               <input
                 id="email"
                 v-model="form.email"
                 name="email"
-                type="email"
+                type="text"
                 required
                 class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               />
