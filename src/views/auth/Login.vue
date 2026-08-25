2a671f221ae38d738e57bdb03e4a3c44fc0e2473
rzranz <rzranz07@gmail.com>
2026-08-25 20:47:22 +0700

    feat: require email for admin and NISN for student

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -139,14 +139,14 @@ const handleLogin = async () => {
         <form class="space-y-6" @submit.prevent="handleLogin">
           <div>
             <label for="email" class="block text-sm font-medium text-slate-700">
-              Email / NISN
+              {{ activeRole === 'admin' ? 'Email address' : 'NISN (Nomor Induk)' }}
             </label>
             <div class="mt-1">
               <input
                 id="email"
                 v-model="form.email"
                 name="email"
-                type="text"
+                :type="activeRole === 'admin' ? 'email' : 'text'"
                 required
                 class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               />
